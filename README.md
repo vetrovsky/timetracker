ba7322af6c3fb8f4b9d63bc80b93f600b141bdb5
Karel Větrovský <vetrovsky@gmail.com>
2023-06-09 19:13:50 +0200

    Add todays stats

--- README.md
+++ README.md
@@ -3,17 +3,24 @@ very simple time tracking app
 
 ## Usage
 
-`python main.py start project_name`
+Print commands list 
+`python main.py help`
 
 Created timestamp to track the *project_name*
-
-`python main.py stop`
+`python main.py start project_name` 
 
 Created timestamp to stop tracking *all* projects
+`python main.py stop`
 
+Print projects list names
 `python main.py list`
 
 List statistics of projects
+`python main.py stats`
+
+Reset works database
+`python main.py delete`
+
 
 ##  Note
 
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
 import os
 import sqlite3
 import sys
@@ -30,34 +31,62 @@ def create_connection(db_file):
     conn = None
     try:
         conn = sqlite3.connect(db_file)
-        print(sqlite3.version)
     except Error as e:
         print(e)
     finally:
         if conn:
             conn.close()
 
-# create a function that returns a list of employees with the given last name
-def get_emps_by_name(lastname):
-    c.execute("SELECT * FROM employees WHERE last_name=:last", {'last': lastname})
+
+#create a function that returns not finished work
+def get_open_project():
+    c.execute("SELECT * FROM works WHERE time_end IS NULL")
     return c.fetchall()
 
-#create a function that returns a list of all employees
-def get_all_emps():
-    c.execute("SELECT * FROM employees")
-    return c.fetchall()
-
-#create a function that returns a list of all test
+#create a function that returns a list of works
 def get_all_works():
     c.execute("SELECT * FROM works")
     return c.fetchall()
 
+# create a function that returns a list of all projects from table works
+def get_all_projects():
+    c.execute("SELECT DISTINCT project FROM works")
+    return c.fetchall()
+
+def stop_all_projects():
+    try:
+        c.execute("UPDATE works SET time_end = :now WHERE time_end IS NULL", {'now': now1})
+    except Error as e:
+        print(e)
+
+# create a function that returns a list of all projects from table works with todays start_date
+def get_todays_projects():
+    today = str(datetime.now().date()) + "%"
+    c.execute("SELECT * FROM works WHERE time_start LIKE :today", {'today': today})
+    return c.fetchall()
+
+# create a function that returns difference in seconds between two timestamps
+def get_diffrence_in_seconds(time_start, time_end):
+    time_start = datetime.strptime(time_start, '%Y-%m-%d %H:%M:%S.%f')
+    time_end = datetime.strptime(time_end, '%Y-%m-%d %H:%M:%S.%f')
+    return (time_end - time_start).total_seconds()
+
+#return hours, minutes and seconds from seconds
+def get_hours_minutes_seconds(seconds):
+    m, s = divmod(seconds, 60)
+    h, m = divmod(m, 60)
+    return h, m, s
+
 def command_help():
     print("help:")
     print("python main.py help")
+    print("python main.py delete")
     print("python main.py start project_name")
     print("python main.py stop")
     print("python main.py list")
+    print("python main.py projects")
+    print("python main.py stats")
+    print("python main.py [opened]")
 
 
 # Press the green button in the gutter to run the script.
@@ -73,6 +102,15 @@ if __name__ == '__main__':
 
     # set database file
     db_file = os.path.join(folder, 'data.db')
+
+    if command == "delete":
+        if os.path.exists(db_file):
+            os.remove(db_file)
+            print("delete file " + db_file)
+        else:
+            print("The file does not exist")
+        exit()
+
     print(f"Open file {db_file} in {folder}")
    # create a database connection
     create_connection(db_file)
@@ -88,38 +126,62 @@ if __name__ == '__main__':
     c.execute("""CREATE TABLE IF NOT EXISTS works (
                 id integer primary key autoincrement,
                 project text,
-                timestamp text
+                time_start text,
+                time_end text
                 )""")
+
+    # create timestamp
+    now1 = str(datetime.now())
+
 
     if command == "help":
         command_help()
+    elif command == "opened":
+        print(get_open_project())
     elif command == "list":
         print(get_all_works())
+    elif command == "projects":
+        print(get_all_projects())
+    elif command == "stop":
+        stop_all_projects()
+        conn.commit()
+        print("stopped projects at " + now1)
+    elif command == "stats":
+        print("todays stats")
+        works =  get_todays_projects()
+        # sum all seconds for every project
+        sum = {}
+        cnt = {}
+        for work in works:
+              sum[work[1]] = sum.get(work[1], 0) + get_diffrence_in_seconds(work[2], work[3] or now1)
+              if work[3] is None:
+                  cnt[work[1]] = work[1]
+
+        # print the key and value for each item in sum
+        for key, val in sum.items():
+            h, m, s = get_hours_minutes_seconds(val)
+
+            print(("* continued * " if key in cnt else "") + key + " " + str(int(h)) + " hodin, " + str(int(m)) + " minut," + str(int(s)) + " sekund")
+
+
     elif command == "start":
-        if len(sys.argv) > 2:
+        if len(sys.argv) < 3:
             print("please enter a project name")
             command_help()
             exit()
         else:
             project = sys.argv[2]
 
-        # insert data into table
-        now = datetime.now()
-        #convert now to string
-        now1 = str(now)
-        c.execute("INSERT INTO works (project, timestamp) VALUES (:project, :now)", {'project': project, 'now': now1});
+
+        # update time_end at last row in table works with now1
+        stop_all_projects()
+
+        c.execute("INSERT INTO works (project, time_start) VALUES (:project, :now)", {'project': project, 'now': now1});
         conn.commit()
         # call the function and print the results
         print("start project: " + project + " at " + now1)
-    elif command == "stop":
-        # insert data into table
-        now = datetime.now()
-        #convert now to string
-        now1 = str(now)
-        c.execute("INSERT INTO works (project, timestamp) VALUES (:project, :now)", {'project': "stop", 'now': now1});
-        conn.commit()
-        # call the function and print the results
-        print("stop project at " + now1)
+    else:
+        print(get_open_project())
 
     # close the connection
     conn.close()
